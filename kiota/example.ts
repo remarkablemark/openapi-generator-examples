984f1c594680262617518503de919d9fd0aad431
Mark <mark@remarkablemark.org>
2026-02-19 00:06:36 -0500

    docs(kiota): add auth to example

--- kiota/example.ts
+++ kiota/example.ts
@@ -1,12 +1,30 @@
-import { AnonymousAuthenticationProvider } from '@microsoft/kiota-abstractions';
+import {
+  AllowedHostsValidator,
+  AuthenticationProvider,
+} from '@microsoft/kiota-abstractions';
 import { FetchRequestAdapter } from '@microsoft/kiota-http-fetchlibrary';
 
 import { createApiClient } from './sdk';
 
-const authProvider = new AnonymousAuthenticationProvider();
+class StaticAuthProvider implements AuthenticationProvider {
+  private readonly allowedHosts = new AllowedHostsValidator(
+    new Set(['api.example.com']),
+  );
 
-const adapter = new FetchRequestAdapter(authProvider);
+  async authenticateRequest(requestInfo, additionalAuthenticationContext) {
+    const url =
+      requestInfo?.uri instanceof URL
+        ? requestInfo.uri
+        : new URL(requestInfo?.uri ?? requestInfo?.url ?? '');
 
+    if (!this.allowedHosts.isUrlHostValid(url)) return;
+
+    requestInfo.headers ??= new Map<string, string>();
+    requestInfo.headers.set('Authorization', `Bearer YOUR_ACCESS_TOKEN`);
+  }
+}
+
+const adapter = new FetchRequestAdapter(new StaticAuthProvider());
 adapter.baseUrl = 'https://api.example.com';
 
 const client = createApiClient(adapter);
