1767503f5409644d22a31b6dd58e7257aa134ab4
Mark <mark@remarkablemark.org>
2026-02-19 01:01:04 -0500

    docs(heyapi): improve type in example

--- heyapi/example.ts
+++ heyapi/example.ts
@@ -1,4 +1,4 @@
-import { getUser } from './sdk';
+import { type GetUserData, getUser } from './sdk';
 import { client } from './sdk/dist/client.gen';
 
 client.setConfig({
@@ -8,7 +8,13 @@ client.setConfig({
   },
 });
 
-getUser({ path: { id: 'user-123' } }).then((user) => {
+const user: Pick<GetUserData, 'path'> = { path: { id: 'user-123' } };
+
+getUser(user).then((user) => {
+  if (user.error) {
+    return console.error(user.error);
+  }
+
   console.log(user.data.email);
   console.log(user.data.id);
   console.log(user.data.name);
